1df85c01f57de5893a5a4e4376da5da0f6a77e28
Lee Colsant <lee.colsant@gmail.com>
2019-06-09 23:40:54 -0700

    deployment files

---
+++ build/precache-manifest.15c54864f3b301a66e51ec623e06e937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3603dd6a12d450313f6278be3329b2b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e843135980ef72915d18",
+    "url": "/static/css/main.0fe93306.chunk.css"
+  },
+  {
+    "revision": "74d70bbf98ec22ae3727",
+    "url": "/static/js/2.c6b3b295.chunk.js"
+  },
+  {
+    "revision": "e843135980ef72915d18",
+    "url": "/static/js/main.21cc60c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
